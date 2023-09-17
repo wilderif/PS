946fef5073e0571ede1f29853d7dfbb78f11fef5
wilderif <73784587+wilderif@users.noreply.github.com>
2023-09-17 17:39:55 +0900

    Create BOJ_1500.c

---
+++ BOJ/BOJ_1500.c
@@ -0,0 +1,31 @@
+// BOJ_1500
+// 최대 곱
+
+#include <stdio.h>
+
+int main()
+{
+    int s, k;
+    long long res = 1;
+    scanf("%d %d", &s, &k);
+
+    int arr[20];
+    for (int i = 0; i < k; ++i) {
+        arr[i] = 1;
+    }
+    for (int i = 0; i < k; ++i) {
+        arr[i] = s / k;
+    }
+
+    for (int i = 0; i < s % k; ++i) {
+        arr[i]++;
+    }
+
+    for (int i = 0; i < k; ++i) {
+        res *= arr[i];
+    }
+
+    printf("%lld", res);
+
+    return 0;
+}
