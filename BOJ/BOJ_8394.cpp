7f339ae7f1bc78b67d9f01ff8debfbf284a37d4f
wilderif <73784587+wilderif@users.noreply.github.com>
2023-11-17 14:28:17 +0900

    Create BOJ_8394.cpp

---
+++ BOJ/BOJ_8394.cpp
@@ -0,0 +1,29 @@
+// BOJ_8394
+// 악수
+
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+
+int main() {
+    int n = 0;
+    cin >> n;
+
+    vector<uint32_t> mem(n + 2, 0);
+    mem[2] = 2;
+    mem[3] = 3;
+
+    for (int i = 4; i < n + 1; ++i) {
+        mem[i] = (mem[i - 1] + mem[i - 2]) % 10;
+    }
+
+    cout << mem[n];
+
+    return 0;
+}
