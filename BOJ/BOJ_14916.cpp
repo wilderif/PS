d3cc6204ee669afa3027603ada07b088c935e755
wilderif <73784587+wilderif@users.noreply.github.com>
2024-01-29 23:40:57 +0900

    Create BOJ_14916.cpp

---
+++ BOJ/BOJ_14916.cpp
@@ -0,0 +1,38 @@
+// BOJ_14916
+// 거스름돈
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n = 0;
+	cin >> n;
+
+	vector<int> mem(n + 10, -1);
+	mem[2] = mem[5] = 1;
+	mem[4] = 2;
+
+	for (int i = 6; i < n + 1; i++) {
+		if (mem[i - 5] != -1 && mem[i - 2] != - 1) {
+			mem[i] = min(mem[i - 5], mem[i - 2]) + 1;
+		}
+		if (mem[i - 5] != -1 && mem[i - 2] == - 1) {
+			mem[i] = mem[i - 5] + 1;
+		}
+		if (mem[i - 5] == -1 && mem[i - 2] != - 1) {
+			mem[i] = mem[i - 2] + 1;
+		}
+	}
+
+	cout << mem[n];
+
+	return 0;
+}
