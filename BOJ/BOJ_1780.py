1f90fdd0129a094be918faeb6f18c46e1d9e9cb0
wilderif <73784587+wilderif@users.noreply.github.com>
2024-02-28 13:19:02 +0900

    Create BOJ_1780.py

---
+++ BOJ/BOJ_1780.py
@@ -0,0 +1,36 @@
+# BOJ_1780
+# 종이의 개수
+
+import sys
+
+
+def divide(x, y, size, arr):
+    global res_w, res_b
+    color = arr[y][x]
+    for j in range(y, y + size):
+        for i in range(x, x + size):
+            if arr[j][i] != color:
+                divide(x, y, size // 2, arr)
+                divide(x + size // 2, y, size// 2 ,arr)
+                divide(x, y + size // 2, size // 2, arr)
+                divide(x + size // 2, y + size // 2, size // 2, arr)
+                return None
+    else:
+        if color:
+            res_b += 1
+        else:
+            res_w += 1
+
+
+def solution():
+    n = int(input())
+    arr = list(list(map(int, sys.stdin.readline().split())) for _ in range(n))
+    global res_w, res_b
+    res_w, res_b = 0, 0
+    divide(0, 0, n, arr)
+    print(res_w)
+    print(res_b)
+
+
+if __name__ == "__main__":
+    solution()
