bbb65f235770ec1f21845f85840db7d3eb56bfb3
wilderif <73784587+wilderif@users.noreply.github.com>
2024-03-04 10:32:48 +0900

    Create BOJ_5397.cpp

---
+++ BOJ/BOJ_5397.cpp
@@ -0,0 +1,52 @@
+// BOJ_5397
+// 키로거
+
+#include <iostream>
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t = 0;
+	cin >> t;
+
+	while (t--) {
+		string l;
+		cin >> l;
+		list<char> container;
+		list<char>::iterator cursor = container.end();
+		for (char c : l) {
+			if (c == '<') {
+				if (cursor != container.begin()) {
+					cursor--;
+				}
+			} else if (c == '>') {
+				if (cursor != container.end()) {
+					cursor++;
+				}
+			} else if (c == '-') {
+				if (cursor != container.begin()) {
+					cursor--;
+					cursor = container.erase(cursor);
+				}
+			} else {
+				container.insert(cursor, c);
+			}
+		}
+		for (char c : container) {
+			cout << c;
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
