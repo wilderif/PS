780cb8c49c79415e9969e8c8543e4d674d339838
wilderif <73784587+wilderif@users.noreply.github.com>
2024-03-20 11:40:08 +0900

    Create BOJ_1629.cpp

---
+++ BOJ/BOJ_1629.cpp
@@ -0,0 +1,34 @@
+// BOJ_1629
+// 곱셈
+
+#include <iostream>
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+uint64_t pow(uint64_t a, uint64_t b, uint64_t c) {
+	if (b == 1) {
+		return a % c;
+	}
+	
+	if (b % 2) {
+		return ((pow(a, b / 2, c) * pow(a, b / 2, c)) % c * (a % c)) % c;
+	} else {
+		return (pow(a, b / 2, c) * pow(a, b / 2, c)) % c;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	uint64_t a, b, c;
+	cin >> a >> b >> c;
+
+	cout << pow(a, b, c);
+
+	return 0;
+}
