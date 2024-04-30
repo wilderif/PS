9eb7e389c82c8bbfd6cde6b41e9d4c779426961b
wilderif <73784587+wilderif@users.noreply.github.com>
2024-04-30 11:26:27 +0900

    Create BOJ_15486.py

---
+++ BOJ/BOJ_15486.py
@@ -0,0 +1,22 @@
+# BOJ_15486
+# 퇴사 2
+
+import sys
+
+inp = sys.stdin.readline
+
+
+def main():
+    n = int(inp())
+    arr = [None] + list(list(map(int, inp().split())) for _ in range(n))
+    mem = [0 for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        mem[i] = max(mem[i - 1], mem[i])
+        target = i + arr[i][0] - 1
+        if target <= n:
+            mem[target] = max(mem[target], mem[i - 1] + arr[i][1])
+    print(mem[n])
+
+
+if __name__ == "__main__":
+    main()
