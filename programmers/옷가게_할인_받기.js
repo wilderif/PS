ece992e7d036222385b50561fac278b1ae31a614
wilderif <73784587+wilderif@users.noreply.github.com>
2024-05-02 00:20:44 +0900

    Create 옷가게_할인_받기.js

---
+++ programmers/옷가게_할인_받기.js
@@ -0,0 +1,16 @@
+// 옷가게_할인_받기
+
+function solution(price) {
+    var answer = 0;
+    if (price >= 500000) {
+        answer = price * 0.8;
+    } else if (price >= 300000) {
+        answer = price * 0.9;
+    } else if (price >= 100000) {
+        answer = price * 0.95;
+    } else {
+        answer = price;
+    }
+
+    return Math.floor(answer);
+}
