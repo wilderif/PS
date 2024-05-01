1c719d5fc1163dcef36c46ffcf128d88ed5df133
wilderif <73784587+wilderif@users.noreply.github.com>
2024-05-01 21:50:21 +0900

    Create 특수문자_출력하기.js

---
+++ programmers/특수문자_출력하기.js
@@ -0,0 +1,11 @@
+// 특수문자_출력하기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\'"<>?:;`);
+});
