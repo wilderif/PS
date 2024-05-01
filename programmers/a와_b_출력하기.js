b60874d796688d87b2a7b8f951f6c79005b8047d
wilderif <73784587+wilderif@users.noreply.github.com>
2024-05-01 21:30:35 +0900

    Create a와_b_출력하기.js

---
+++ programmers/a와_b_출력하기.js
@@ -0,0 +1,16 @@
+// a와_b_출력하기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log(`a = ${Number(input[0])}`);
+    console.log(`b = ${Number(input[1])}`);
+});
