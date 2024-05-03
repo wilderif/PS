5a009bf247adab4244d262b40b955de0347fd667
wilderif <73784587+wilderif@users.noreply.github.com>
2024-05-04 02:27:51 +0900

    Create 개미_군단.js

---
+++ programmers/개미_군단.js
@@ -0,0 +1,16 @@
+// 개미_군단
+
+function solution(hp) {
+  var answer = 0;
+  let mem = new Array(1001).fill(0);
+  mem[1] = 1;
+  mem[2] = 2;
+  mem[3] = 1;
+  mem[4] = 2;
+  mem[5] = 1;
+  for (let i = 6; i < 1001; i++) {
+    mem[i] = Math.min(mem[i - 1], mem[i - 3], mem[i - 5]) + 1;
+  }
+  answer = mem[hp];
+  return answer;
+}
