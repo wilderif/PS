32662b9223223402cf87e5272fdb516fbe5d22eb
wilderif <73784587+wilderif@users.noreply.github.com>
2024-05-03 15:00:55 +0900

    Create 문자_반복_출력하기.js

---
+++ programmers/문자_반복_출력하기.js
@@ -0,0 +1,11 @@
+// 문자_반복_출력하기
+
+function solution(my_string, n) {
+    var answer = '';
+    for (let c of my_string) {
+        for (let i = 0; i < n; i++) {
+            answer += c;
+        }
+    }
+    return answer;
+}
