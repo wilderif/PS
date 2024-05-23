a39319dde23de02211f4756b0807c7bad5c53d29
wilderif <woojuro3@gmail.com>
2024-05-23 21:41:11 +0900

    Modified check

--- programmers/a와_b_출력하기.js
+++ programmers/a와_b_출력하기.js
@@ -1,20 +1,16 @@
 // a와_b_출력하기
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    console.log(`a = ${Number(input[0])}`);
-    console.log(`b = ${Number(input[1])}`);
-<<<<<<< HEAD
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  console.log(`a = ${Number(input[0])}`);
+  console.log(`b = ${Number(input[1])}`);
 });
-=======
-});
->>>>>>> 5ae11d8804eca0ee95680c2228111cdb04fcc3ac
