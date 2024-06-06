c62cd51e9bab5b2113e684a5a0d7afeca650f3c6
wilderif <woojuro3@gmail.com>
2024-06-06 15:41:45 +0900

    Modify BOJ_11404.py

--- BOJ/BOJ_11404.py
+++ BOJ/BOJ_11404.py
@@ -13,17 +13,12 @@ def main():
     graph = [[math.inf for _ in range(n + 1)] for _ in range(n + 1)]
     for _ in range(m):
         a, b, c = map(int, inp().split())
-        if graph[a][b] > c:
-            graph[a][b] = c
+        graph[a][b] = min(graph[a][b], c)
+    
     for i in range(1, n + 1):
         for j in range(1, n + 1):
-            if i == j:
-                continue
             for k in range(1, n + 1):
-                if j == k:
-                    continue
-                if graph[j][k] > graph[j][i] + graph[i][k]:
-                    graph[j][k] = graph[j][i] + graph[i][k]
+                graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
 
     for i in range(1, n + 1):
         for j in range(1, n + 1):
