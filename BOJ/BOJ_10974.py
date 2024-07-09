c0a16d7c35feee8b7938871e1eb4009e04aa00d9
wilderif <woojuro3@gmail.com>
2024-07-09 17:24:35 +0900

    Create BOJ_10974.py

--- BOJ/BOJ_10974.py
+++ BOJ/BOJ_10974.py
@@ -6,25 +6,26 @@ import sys
 inp = sys.stdin.readline
 
 
-def back(depth):
+def backtracking(depth):
     if depth == n:
         print(*out)
         return
     for i in range(n):
         if not vis[i]:
             vis[i] = True
-            out[depth] = i + 1
-            back(depth + 1)
+            out.append(i + 1)
+            backtracking(depth + 1)
             vis[i] = False
+            out.pop()
 
 
 def main():
     global n, vis, out
     n = int(inp())
     vis = [False for _ in range(n)]
-    out = [0 for _ in range(n)]
-    back(0)
+    out = []
+    backtracking(0)
 
-
+                
 if __name__ == "__main__":
     main()
