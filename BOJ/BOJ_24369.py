719a7a6bf5da83b53d7ede97c0567a035b7c2af8
wilderif <woojuro3@gmail.com>
2024-08-26 11:10:48 +0900

    Create BOJ_24369.py

---
+++ BOJ/BOJ_24369.py
@@ -0,0 +1,36 @@
+# BOJ_24369
+# 알고리즘 수업 - 점근적 표기 5
+
+import sys
+
+inp = sys.stdin.readline
+
+
+def main():
+    a2, a1, a0 = map(int, inp().split())
+    c = int(inp())
+    n = int(inp())
+
+    if a2 < c:
+        print(0)
+        return
+    elif a2 == c:
+        if -a1 * n - a0 <= 0:
+            print(1)
+            return
+        print(0)
+    else:
+        v = a1 / (2 * (c - a2))
+        if v <= n:
+            if (c - a2) * n ** 2 - a1 * n - a0 <= 0:
+                print(1)
+                return
+        else:
+            if (c - a2) * v ** 2 - a1 * v - a0 <= 0:
+                print(1)
+                return
+        print(0)
+
+
+if __name__ == "__main__":
+    main()
