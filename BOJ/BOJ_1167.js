8318d3acc2d7d89e6d929d7749fffa5aea7b77be
wilderif <woojuro3@gmail.com>
2025-01-15 10:24:32 +0900

    Solve BOJ_1167.js

---
+++ BOJ/BOJ_1167.js
@@ -0,0 +1,53 @@
+// BOJ_1167
+// 트리의 지름
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+function solution(inputArr) {
+  const n = Number(inputArr[0]);
+  const graph = Array.from({ length: n + 1 }, () => new Array());
+  for (const el of inputArr.splice(1)) {
+    const tmp = el.split(' ').map(Number);
+    const v1 = tmp[0];
+    for (let i = 1; i < tmp.length - 1; i++) {
+      graph[v1].push([tmp[i], tmp[++i]]);
+    }
+  }
+
+  const dfs = (node) => {
+    const vis = new Array(n + 1).fill(false);
+    const stack = [[node, 0]];
+    vis[node] = true;
+    const ret = [-1, -1];
+
+    while (stack.length) {
+      const [cur, curW] = stack.pop();
+      let isLeaf = true;
+      for (const [nxt, nxtW] of graph[cur]) {
+        if (vis[nxt]) {
+          continue;
+        }
+        stack.push([nxt, curW + nxtW]);
+        vis[nxt] = true;
+        isLeaf = false;
+      }
+
+      if (isLeaf) {
+        if (ret[1] < curW) {
+          ret[0] = cur;
+          ret[1] = curW;
+        }
+      }
+    }
+
+    return ret;
+  };
+
+  const startNode = dfs(1)[0];
+
+  return dfs(startNode)[1];
+}
+
+console.log(solution(input));
